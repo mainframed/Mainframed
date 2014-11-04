b65117f2149b2c01f7aa7b9eaff46904b6b6acac
mainframed <mainframed767@gmail.com>
2014-11-03 21:38:17 -0800

    Create create_pds.py
    Wanted to make some partitioned datasets. Got tired of having to do that shit in ISPF.

---
+++ create_pds.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+print "[~] Super cheesy script to make PDS's, cause fuck ISPF, thats why "
+print "[~] mkdir pds_name to create file                                                        "
+print "[~] puts an empty file in the pds (or member you pedantic)                               \n"
+print "[!] takes three arguments: hostname username password pds_name"
+print "[!] i.e. ./create_pds.py emc plague secret HAXORS "
+print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
+
+debug = False
+
+import ftplib
+import cStringIO
+import sys
+
+print "hostname", sys.argv[1]
+hostname = sys.argv[1]
+print "usnermae", sys.argv[2]
+username = sys.argv[2]
+print "password ********" #LOL
+password = sys.argv[3]
+print "filename", sys.argv[4]
+filename = sys.argv[4]
+if len(sys.argv) >= 6:
+	if sys.argv[5] == "yes": debug = True
+
+ftp = ftplib.FTP(hostname)
+
+if debug: print ftp.getwelcome()
+print "\n weeeeeee\n"
+ftp.login(username, password)
+#print ftp.sendcmd('site lr=32000 blk=32000 directory=25 primary=1500 secondary=250 cy')
+print "[+] making pds", filename
+d = ftp.sendcmd('mkd '+filename)
+if debug: print d
+d = ftp.cwd(filename)
+if debug: print d
+voidfile = cStringIO.StringIO('')
+print "[+] creating EMPTY member in pds", filename
+
+d = ftp.storbinary('STOR EMPTY', voidfile)
+if debug: print d
+
+ftp.close()
+voidfile.close()
