5521c12eb072842d31e5594dcb4ba2cf62a03038
mainframed <mainframed767@gmail.com>
2015-03-22 02:30:33 -0700

    Update mac_os_x_networking.sh
    Making the script better
    - adding Notorious B.I.G quote
    - added ping check to see if the host is up

--- mac_os_x_networking.sh
+++ mac_os_x_networking.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # By Soldier of Fortran
 # Sets the networking right on MAC OS X Yosemite
-# Get z/OS and F0C batches
+# F*ck batches, get mainframes
 
 # This script will be asking for your sudo password. 
-# I hope you read the script and know what it's doing.
+# I hope you read the script and know what it's doing
+# Instead of just typing it in.
 
 
 #Change these to match your setup:
@@ -17,14 +18,18 @@ MAC_MAC=`ifconfig en0 | grep -Eo '([[:xdigit:]]{1,2}[:-]){5}[[:xdigit:]]{1,2}' |
 
 grn=$'\e[1;32m'
 end=$'\e[0m'
+red=$'\e[1;31m'
+mag=$'\e[1;35m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+cyn=$'\e[1;36m'
 
-#
-#source: http://www.retrojunkie.com/asciiart/animals/dinos.htm
-#
+###
+#Source: http://www.retrojunkie.com/asciiart/animals/dinos.htm
+###
 echo $grn
 
 cat <<'EOF'
-
           /\      ,|
          :  \  /\/ |   _
      .\  |   :: /  | ,' |
@@ -46,23 +51,33 @@ cat <<'EOF'
   :  _ | \   )   / _  |:   : `._)_,)/
    )   | :`  |   :    ;| . ).__'_.-'
    :   ) ; _ ;   |   / ; _ ;
-   ) - \ `^^^'   ; - \ `^^' SSt
+   ) - \ `^^^'   ; - \ `^^' 
    '^^^`         `^^^'
-    Watch out for the ZtegOSaurus
+   Watch out for the ZtegOSaurus
+
 EOF
 
+# From: Hours of screwing around getting this to work
 echo "  [+] Setting up IFCONFIG properlly this time: $MAC_IP --> $HERC_IP"
 sudo ifconfig tun0 $MAC_IP $HERC_IP
 
-# from https://github.com/vapier/hercules-390-hyperion/blob/master/README.OSX
-
+#From: https://github.com/vapier/hercules-390-hyperion/blob/master/README.OSX
 echo "  [+] Setting up IP Forwarding on $MAC_IP"
-sudo sysctl -w net.inet.ip.forwarding=1
+sudo sysctl -w net.inet.ip.forwarding=1 >/dev/null
+echo "  [+] Setting up ARP on MAC Address: $MAC_MAC"
+sudo arp -s $HERC_IP $MAC_MAC pub en0 >/dev/null
 
-echo "  [+] Setting up ARP forwarding on MAC Address: $MAC_MAC"
+#Okay, now check the machine is up
+ping -c 1 $HERC_IP &>/dev/null
+if [ $? -ne 0 ] ; then
+   echo "$red  [!] $HERC_IP isn't pingable.$yel You did something wrong!"
+   echo $grn
+   echo "      Did you IPL the system already? Is TCPIP running in z/OS?"
+   echo "      Did you setup CTCI properly? Is hercules running?"
+   echo "      You$red MUST$grn do all of those before this will work" 
+else
+   # Let's make sure we use the ugliest colors here
+   echo "  [+] $HERC_IP is up!$mag You$yel did$cyn it!"
+fi
 
-## !! Change 'en0' below to your network adapter. Should be fine though !!
-sudo arp -s $HERC_IP $MAC_MAC pub en0
-
-echo "  [+] Done! Try pinging $HERC_IP"
 echo $end
